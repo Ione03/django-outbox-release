0864b919b7c63f67d5c5273116564b7a028c2789
Ione03 <suratiwan03@gmail.com>
2022-11-30 13:15:10 +0800

    fix error on backend processor

--- backend/processor.py
+++ backend/processor.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext as _
 from core.models import User
 from django.http import Http404
 def get_menu_group(user_id):
-	A=User.objects.filter(id=user_id);print('menugroup = ',A)
-	if A:print('menugroup[0].id = ',A[0].menu_group.id);return A[0].menu_group.id
+	A=User.objects.filter(id=user_id)
+	if A:
+		if A[0].menu_group:return A[0].menu_group.id
+	return None
 def context_outbox(request):A=request;B=get_site_id(A);return{'menugroup':get_menu_group(A.user.id)}
