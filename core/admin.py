60e6e43996e52dcd2b826fe372e219b79c552589
manjaro-linux <suratiwan03@gmail.com>
2023-06-17 03:36:58 +0800

    Add User to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,14 @@
-_O='template'
-_N='description'
-_M='last_login'
-_L='groups'
-_K='is_superuser'
-_J='file_path'
-_I='model_list'
-_H='kind'
-_G='is_staff'
-_F='is_active'
+_P='template'
+_O='description'
+_N='last_login'
+_M='groups'
+_L='is_superuser'
+_K='file_path'
+_J='model_list'
+_I='kind'
+_H='is_staff'
+_G='is_active'
+_F='agency'
 _E='email'
 _D='fields'
 _C='-updated_at'
@@ -19,18 +20,20 @@ from django.utils.translation import gettext_lazy as _
 from parler.admin import TranslatableAdmin
 from .models import *
 from .forms import *
+from django.contrib.auth import get_user_model
+User=get_user_model()
 admin.site.register(Agency,TranslatableAdmin)
-class BaseUserAdmin(UserAdmin):fieldsets=(None,{_D:(_E,'password')}),(_('Personal info'),{_D:(_A,)}),(_('Permissions'),{_D:(_F,_G,_K,_L,'user_permissions')}),(_('Important dates'),{_D:(_M,)});add_fieldsets=(None,{'classes':('wide',),_D:(_E,_A,'password1','password2')}),;list_display=_E,_A,_G,_M;list_filter=_G,_K,_F,_L;search_fields=_E,_A;ordering=_E,
+class BaseUserAdmin(UserAdmin):fieldsets=(None,{_D:(_E,'password')}),(_('Personal info'),{_D:(_A,_F)}),(_('Permissions'),{_D:(_G,_H,_L,_M,'user_permissions')}),(_('Important dates'),{_D:(_N,)});add_fieldsets=(None,{'classes':('wide',),_D:(_E,_A,'password1','password2',_F)}),;list_display=_E,_A,_H,_N;list_filter=_H,_L,_G,_M;search_fields=_E,_A;ordering=_E,
 admin.site.register(User,BaseUserAdmin)
-class ServiceAdmin(admin.ModelAdmin):list_filter=_H,;list_display=[_H,'agency',_F,'site','expired_date',_B];search_fields=_H,;ordering=_C,
+class ServiceAdmin(admin.ModelAdmin):list_filter=_I,;list_display=[_I,_F,_G,'site','expired_date',_B];search_fields=_I,;ordering=_C,
 admin.site.register(Service,ServiceAdmin)
 class TemplateAdmin(admin.ModelAdmin):list_filter=_A,;list_display=[_A,'rel_path',_B];search_fields=_A,;ordering=_C,
 admin.site.register(Template,TemplateAdmin)
 class TemplateOwnerAdmin(admin.ModelAdmin):list_filter=_A,;list_display=[_A,_B];search_fields=_A,;ordering=_C,
 admin.site.register(TemplateOwner,TemplateOwnerAdmin)
-class ModelListAdmin(admin.ModelAdmin):list_filter=_A,_N,'status';list_display=[_A,_N,_B];search_fields=_A,;ordering=_C,
+class ModelListAdmin(admin.ModelAdmin):list_filter=_A,_O,'status';list_display=[_A,_O,_B];search_fields=_A,;ordering=_C,
 admin.site.register(ModelList,ModelListAdmin)
-class ModelListSettingAdmin(admin.ModelAdmin):list_filter=_I,_O;list_display=[_I,_O,_B];search_fields=_I,;ordering=_C,
+class ModelListSettingAdmin(admin.ModelAdmin):list_filter=_J,_P;list_display=[_J,_P,_B];search_fields=_J,;ordering=_C,
 admin.site.register(ModelListSetting,ModelListSettingAdmin)
-class PhotoAdmin(admin.ModelAdmin):list_filter=_J,;list_display=[_J,_B];search_fields=_J,;ordering=_C,
+class PhotoAdmin(admin.ModelAdmin):list_filter=_K,;list_display=[_K,_B];search_fields=_K,;ordering=_C,
 admin.site.register(Photo,PhotoAdmin)
